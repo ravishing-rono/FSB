7cbbdb9144ac02fe51f2142a1127e1fa890616c5
Ravishing Rono <133150284+ravishing-rono@users.noreply.github.com>
2023-11-13 09:30:56 +0600

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Official file bot of @animephile</b>",
+            text = f"<b>○ Official file bot of @animephile0</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
